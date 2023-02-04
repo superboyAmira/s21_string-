4d15ab0ff0e0c59d2438f6ba4b9317d2ae5494ba
Superboy Amira <superboy@il-f6.msk.21-school.ru>
2023-02-04 19:47:59 +0300

    memcpy done

--- src/lib/copy.c
+++ src/lib/copy.c
@@ -1,21 +1,26 @@
 #include "s21_string.h"
 
-void *memcpy(void *dest, const void *src, size_t n) { // Копирует n символов из src в dest.
-    
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) { // Копирует n символов из src в dest.
+    if (dest != s21_NULL && src != s21_NULL) {
+        for (s21_size_t i = 0; i < n; i++) {
+            ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+        }
+    }
+    return dest;
 }
 
-void *memmove(void *dest, const void *src, size_t n) { // Еще одна функция для копирования n символов из src в dest.
+void *s21_memmove(void *dest, const void *src, s21_size_t n) { // Еще одна функция для копирования n символов из src в dest.
+   
+}
+
+void *s21_memset(void *str, int c, s21_size_t n) { // Копирует символ c (беззнаковый тип) в первые n символов строки, на которую указывает аргумент str.
 
 }
 
-void *memset(void *str, int c, size_t n) { // Копирует символ c (беззнаковый тип) в первые n символов строки, на которую указывает аргумент str.
+char *s21_strcpy(char *dest, const char *src) { // Копирует строку, на которую указывает src, в dest.
 
 }
 
-char *strcpy(char *dest, const char *src) { // Копирует строку, на которую указывает src, в dest.
-
-}
-
-char *strncpy(char *dest, const char *src, size_t n) { // Копирует до n символов из строки, на которую указывает src, в dest.
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) { // Копирует до n символов из строки, на которую указывает src, в dest.
 
 }
