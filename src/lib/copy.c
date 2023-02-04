3d6f6096bcbe5d18c464eb6dbd32d8d63174d5c7
Superboy Amira <superboy@il-f6.msk.21-school.ru>
2023-02-04 19:17:36 +0300

    Make develop part 2, need repair

---
+++ src/lib/copy.c
@@ -0,0 +1,21 @@
+#include "s21_string.h"
+
+void *memcpy(void *dest, const void *src, size_t n) { // Копирует n символов из src в dest.
+    
+}
+
+void *memmove(void *dest, const void *src, size_t n) { // Еще одна функция для копирования n символов из src в dest.
+
+}
+
+void *memset(void *str, int c, size_t n) { // Копирует символ c (беззнаковый тип) в первые n символов строки, на которую указывает аргумент str.
+
+}
+
+char *strcpy(char *dest, const char *src) { // Копирует строку, на которую указывает src, в dest.
+
+}
+
+char *strncpy(char *dest, const char *src, size_t n) { // Копирует до n символов из строки, на которую указывает src, в dest.
+
+}
---
+++ src/s21_string.h
@@ -0,0 +1,8 @@
+#ifndef SRC_S21_STRING_H
+#define SRC_S21_STRING_H_
+
+#include <stdio.h>
+// #include <string.h>
+#include <math.h>
+
+#endif  // SRC_S21_STRING_H_
