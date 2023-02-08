fed01c2e84785a8e6d41d212dffd70fb35b2223e
Superboy Amira <superboy@il-f6.msk.21-school.ru>
2023-02-08 16:54:35 +0300

    ready build

--- src/lib/strtok.c
+++ src/lib/strtok.c
@@ -1,13 +1,13 @@
 #include "../s21_string.h"
-
+#include <string.h>
 char *s21_strtok(char *str, const char *delim) { // Разбивает строку str на ряд токенов, разделенных delim.
   static char* mem = s21_NULL;
 
-  if (str) mem = str;
+  if (str != s21_NULL) mem = str;
 
-  if (mem) {
-    str = mem + s21_strspn(mem, delim);
-    mem = str + s21_strcspn(str, delim);
+  if (mem != s21_NULL) {
+    str = mem + strspn(mem, delim);
+    mem = str + strcspn(str, delim);
     if (*mem != '\0') {
       *mem = '\0';
       ++mem;
--- src/test_files/s21_test_string.h
+++ src/test_files/s21_test_string.h
@@ -4,11 +4,4 @@
 #include "../s21_string.h"
 #include <check.h>
 
-#define s21_size_t unsigned long long
-#define s21_NULL (void*)0 
-#define flag short int
-#define TRUE 1
-#define FALSE 0
-
-
 #endif  // SRC_TEST_S21_TEST_STRING_H
