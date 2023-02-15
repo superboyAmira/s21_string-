b28f2201d47c013d1140a6a25763138f0cd72d6e
Superboy Amira <superboy@il-f6.msk.21-school.ru>
2023-02-15 12:44:44 +0300

    ready %s sprintf.c

--- src/lib/sprintf.c
+++ src/lib/sprintf.c
@@ -6,25 +6,47 @@
 
 void reverse(char string[], int length);
 char *s21_itoa_decimal(int num, char *str, int place);
-
+// long double float_res(input);
 
 int s21_sprintf(char *str, const char *format, ... ) {
-    int cnt_num = 0; // if error -1
-
+    int cnt_num = 0;
     va_list(input);
     va_start(input, format);
-    for (int chr = 0; chr < strlen(format); chr++) {
-        if (format[chr] == '%') {
-            if (format[chr + 1] == 'd' && format[chr + 1] == 'i') {
+    
+    for (int chr_format = 0, chr_str = 0; format[chr_format] != '\0'; chr_format++) {
+        if (format[chr_format] == '%') {
+            if (format[chr_format + 1] == 'd' || format[chr_format + 1] == 'i') {
                 int param = va_arg(input, int);
-                s21_itoa_decimal(param, str, chr);
-                chr++;
+                s21_itoa_decimal(param, str, chr_str);
+                chr_format++;
+                chr_str++;
+                cnt_num++;
+            } else if (format[chr_format + 1] == 'c') {
+                char param = (char)va_arg(input, int); // https://ru.manpages.org/va_arg/3
+                str[chr_format] = param;
+                chr_str++;
+                cnt_num++;
+            // } else if (format[chr+1]) == 'f' {
+            //     float_res(input);
+            } else if (format[chr_format + 1] == 's') {
+                char *param = va_arg(input, char *);
+                strcat(str, param);
+                chr_str = strlen(str) - 1;
+                chr_format++;
+                cnt_num++;
+            // } else if (format[chr+1]) == 'u' { // беззнаковое число
+            //     int param = va_arg(input, int);
+            } else if (format[chr_format + 1] == '%') {
+                str[chr_str] = '%';
+                chr_format++;
                 cnt_num++;
             } else {
-                str[chr] = '%';
+                printf("Non-valid Format", stderr);
+                break; // если после % непонятно что, то программа ломается
             }
         } else {
-            str[chr] = format [chr];
+            str[chr_str] = format[chr_format];
+            chr_str++;
         }
     }
     va_end(input);
@@ -74,12 +96,24 @@ void reverse(char string[], int length)
         string[start++] = tmp[end--];
     }
 }
- 
+
+// long double float_res(input) {
+//     long double val = va_arg(input, float);
+//     long double left = 0;
+//     long double right = modfl(val, &left); // разбивает число на целую и дробную части; целая часть помещается в left; возвращается дробная часть
+//     long long round_left = left;
+//     int precision = ;
+//     for (int i = 0; i < precision; i++) {
+//         right *= 10;
+//     }
+//     right = roundl(right); // округляем right
+//     return ;
+// }
 
 int main (void) {
-    char dest2[6]="";
+    char dest2[100];
 
-    s21_sprintf(dest2, "abc %d%d", 12, 2);
+    s21_sprintf(dest2, "abc %s", "Hello");
     printf ("src: %s", dest2);
 
    return 0;
